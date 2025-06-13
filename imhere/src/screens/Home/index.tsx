cfcde6a0fce696b5d7438f9194682c2a7e232a25
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-06-13 01:15:35 -0300

    01-fundamentos[03-Finalizando o App] - Finalizado

--- imhere/src/screens/Home/index.tsx
+++ imhere/src/screens/Home/index.tsx
@@ -8,37 +8,39 @@ import {
 } from "react-native";
 import Participant from "../../components/Participant";
 import { styles } from "./style";
+import { useState } from "react";
 
 export default function Home() {
-  const participants = [
-    "Rodrigo",
-    "Vanderson",
-    "Vanessa",
-    "Angelita",
-    "Márcio",
-    "Emily",
-    "Bryan",
-    "Felipe",
-    "João",
-    "Eduardo",
-    "Ravi",
-    "Rafael",
-  ];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState("");
 
   function handleParticipantAdd() {
-    if (participants.includes("Rodrigo")) {
+    if(!participantName) {
+      return Alert.alert(
+        "Atenção!",
+        "Adicione um nome"
+      );
+    }
+
+    if (participants.includes(participantName)) {
       return Alert.alert(
         "Participante Existe",
         "Já existe um participante na lista com esse nome"
       );
     }
+
+    setParticipants((prevState) => [...prevState, participantName]);
+    setParticipantName("");
   }
 
   function handleParticipantRemove(name: string) {
     Alert.alert("Remover", `Remover o participante ${name} ?`, [
       {
         text: "Sim",
-        onPress: () => Alert.alert(`Participante ${name} Deletado(a)!`),
+        onPress: () =>
+          setParticipants((prevState) =>
+            prevState.filter((participant) => participant !== name)
+          ),
       },
       {
         text: "Não",
@@ -58,6 +60,8 @@ export default function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6B6B6B"
+          onChangeText={setParticipantName}
+          value={participantName}
         />
 
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
